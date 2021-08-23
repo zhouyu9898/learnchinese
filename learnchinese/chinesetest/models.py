76676878957dfe76b3eaaa963b188052b721278b
zhouyu9898 <zhouyuzhouyu98@gmail.com>
2021-08-23 22:53:13 +0200

    initial push

---
+++ learnchinese/chinesetest/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Word(models.Model):
+    hanzi = models.CharField(max_length=30)
+    pinyin = models.CharField(max_length=30)
+    meaning = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.hanzi
