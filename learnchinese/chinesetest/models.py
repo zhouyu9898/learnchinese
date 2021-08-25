fd4f3111091ae534a6ba7db89d13bd387cf4bf46
zhouyu9898 <zhouyuzhouyu98@gmail.com>
2021-08-25 19:54:58 +0200

    basic form working, template if not working

--- learnchinese/chinesetest/models.py
+++ learnchinese/chinesetest/models.py
@@ -5,6 +5,7 @@ class Word(models.Model):
     hanzi = models.CharField(max_length=30)
     pinyin = models.CharField(max_length=30)
     meaning = models.CharField(max_length=100)
+    solved = models.BooleanField(default=False)
 
     def __str__(self):
         return self.hanzi
--- learnchinese/chinesetest/views.py
+++ learnchinese/chinesetest/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Word
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the chinesetest index.")
+    words = Word.objects.all()
+    context = {'words_list': words}
+    return render(request, 'index.html', context)
+
+
+def check(request):
+    w = Word.objects.filter(id=int(request.POST['word_id']))
+    if w[0].pinyin == request.POST['pinyin']:
+        w.update(solved=True)
+
+    words = Word.objects.all()
+    context = {'words_list': words}
+    return render(request, 'index.html', context)
