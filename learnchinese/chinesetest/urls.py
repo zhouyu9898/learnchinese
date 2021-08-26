a3ce1ddb316ea1c7b02a44e1e82568ce965e2d93
zhouyu9898 <zhouyuzhouyu98@gmail.com>
2021-08-26 19:28:38 +0200

    solved, reset and shuffle buttons working

--- learnchinese/chinesetest/urls.py
+++ learnchinese/chinesetest/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('check', views.check, name='check'),
+    path('reset', views.reset, name='reset'),
+    path('shuffle', views.shuffle, name='shuffle'),
 ]
--- learnchinese/chinesetest/views.py
+++ learnchinese/chinesetest/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Word
+import random
 
 
 def index(request):
@@ -15,5 +16,21 @@ def check(request):
         w.update(solved=True)
 
     words = Word.objects.all()
+    context = {'words_list': words}
+    return render(request, 'index.html', context)
+
+
+def reset(request):
+    words = Word.objects.all()
+    words.update(solved=False)
+
+    context = {'words_list': words}
+    return render(request, 'index.html', context)
+
+
+def shuffle(request):
+    words = list(Word.objects.all())
+    random.shuffle(words)
+    print(words)
     context = {'words_list': words}
     return render(request, 'index.html', context)
