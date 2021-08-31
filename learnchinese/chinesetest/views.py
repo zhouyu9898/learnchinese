b23def633e6e1361e826b681efb9441177c802ee
zhouyu9898 <zhouyuzhouyu98@gmail.com>
2021-08-31 19:10:56 +0200

    check working, index.html with check, returning sorted word list

--- learnchinese/chinesetest/views.py
+++ learnchinese/chinesetest/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Word
 import random
+import unidecode
 
 
 def index(request):
-    words = list(Word.objects.all())
-    print(type(words))
+    words = Word.objects.all().order_by('pinyin')
     context = {'words_list': list(words)}
     return render(request, 'index.html', context)
 
 
 def check(request):
     w = Word.objects.filter(id=int(request.POST['word_id']))
-    if w[0].pinyin == request.POST['pinyin']:
+    if unidecode.unidecode(w[0].pinyin).replace(" ", "").lower() == request.POST['pinyin']:
         w.update(solved=True)
 
-    words = Word.objects.all()
+    words = Word.objects.all().order_by('pinyin')
     context = {'words_list': words}
     return render(request, 'index.html', context)
 
