c695c68ed6fe6716494cf061853d6d5d8664315c
Zhouyu <zhouyuzhouyu98@gmail.com>
2022-09-09 15:33:44 +0200

    added Solved model, look at paper design for Word, Solved and User

--- learnchinese/chinesetest/models.py
+++ learnchinese/chinesetest/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
 class Word(models.Model):
     hanzi = models.CharField(max_length=30)
     pinyin = models.CharField(max_length=30)
@@ -10,3 +11,6 @@ class Word(models.Model):
 
     def __str__(self):
         return self.hanzi + " " + self.pinyin
+
+class Solved(models.Model):
+    pass
