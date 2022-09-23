5e37c34640cedca4011554765486a15e9be4013d
Zhouyu <zhouyuzhouyu98@gmail.com>
2022-09-23 14:03:20 +0200

    removed solved field from Word and added Solve model to handle user's solved words

--- learnchinese/chinesetest/models.py
+++ learnchinese/chinesetest/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Word(models.Model):
     hanzi = models.CharField(max_length=30)
     pinyin = models.CharField(max_length=30)
     meaning = models.CharField(default='ok', max_length=100)
-    solved = models.BooleanField(default=False)
     hsk = models.IntegerField()
 
     def __str__(self):
         return self.hanzi + " " + self.pinyin
 
-class Solved(models.Model):
-    pass
+class Solve(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    word = models.ForeignKey("Word", on_delete=models.CASCADE)
